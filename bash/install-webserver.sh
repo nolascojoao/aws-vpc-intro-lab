6aad2602cc72219626c3f4a770ac667f374548ce
unknown <nolascojoaops@gmail.com>
2024-09-23 18:04:44 -0300

    Fix web server installation script

--- bash/install-webserver.sh
+++ bash/install-webserver.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#Install Apache Web Server and PHP
-yum install -y httpd mysql php
-#Download Lab files
-wget https://aws-tc-largeobjects.s3.us-west-2.amazonaws.com/CUR-TF-100-RESTRT-1/267-lab-NF-build-vpc-web-server/s3/lab-app.zip
-unzip lab-app.zip -d /var/www/html/
-#Turn on web server
-chkconfig httpd on
-service httpd start
+# Installs the Apache web server            
+yum -y install httpd
+# Configures httpd to start on boot      
+systemctl enable httpd
+# Starts the httpd service now    
+systemctl start httpd
+# Creates an HTML homepage
+echo '<html><h1>Hello From Your Web Server!</h1></html>' > /var/www/html/index.html 
